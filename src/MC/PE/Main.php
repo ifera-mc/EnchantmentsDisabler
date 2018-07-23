addc2e3149c5c15d0771876ca632b848cd55ec5c
Saini14 TV <38633228+Saini14@users.noreply.github.com>
2018-07-23 08:00:52 -0700

    Create Main.php

---
+++ src/MC/PE/Main.php
@@ -0,0 +1,31 @@
+<?php
+namespace MC\PE;
+use pocketmine\utils\config;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+/**
+ * S a i n i 1 4
+ */
+/**
+ * Class Main
+ * @package MC\PE
+ */
+class Main extends PluginBase
+{
+	/**@var config */
+	public $cfg;
+	public function onEnable()
+	{
+		if (!is_dir($this->getDataFolder())) {
+			mkdir($this->getDataFolder());
+		}
+		$this->saveDefaultConfig();
+		/** @var \PiggyCustomEnchants\Main $ce */
+		$ce = $this->getServer()->getPluginManager()->getPlugin("PiggyCustomEnchants");
+		$ids = $this->getConfig()->get("ids");
+		foreach ($ids as $id) {
+			$ce->unregisterEnchantment($id);
+		}
+	}
+}
